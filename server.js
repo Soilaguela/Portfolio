4e4b6b8ccd860eb64d0a9307a36b826b98b1d101
Soilaguela <soilabenguela@gmail.com>
2024-12-04 20:25:43 +0000

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post("/contact", (req, res) => {
 });
 
 // Inicializar o servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
