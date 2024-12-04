5439b0ca6a041513f8ae76909a41ab171fa7c5d8
Soilaguela <soilabenguela@gmail.com>
2024-12-04 16:07:40 +0000

    Mudar os caminhos.
    Mudar os caminhos  para pasta html.

--- About.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const progressBars = document.querySelectorAll(".progress");
-
-    progressBars.forEach(bar => {
-        const level = bar.getAttribute("data-level");
-        bar.style.width = level;
-    });
-});
